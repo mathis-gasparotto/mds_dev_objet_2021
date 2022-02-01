5da9d058e66f7c412ef76e492184481fd4e2195e
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2022-02-01 17:25:14 +0100

    Affichage des tâches

--- ToDoList/index.php
+++ ToDoList/index.php
@@ -13,3 +13,35 @@ var_dump("Affichage des tâches complétées >", $todoList->showCompleted());
 var_dump("Recherche de titre1 >", $todoList->search("titre1"));
 var_dump("Recherche de titre2 >", $todoList->search("titre2"));
 var_dump("Recherche de titre3 >", $todoList->search("titre3"));
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>ToDo List PHP</title>
+</head>
+<body>
+<ul>
+    <h2>A faire</h2>
+    <?php
+    foreach ($todoList->showCompleted() as $todo) { ?>
+        <li>
+            <?php echo $todo->title." - ".$todo->description; ?>
+        </li>
+    <?php } ?>
+
+</ul>
+<ul>
+    <h2>Déjà fait</h2>
+    <?php
+    foreach ($todoList->showNotCompleted() as $todo) { ?>
+        <li>
+            <?php echo $todo->title." - ".$todo->description; ?>
+        </li>
+    <?php } ?>
+
+</ul>
+</body>
+</html>
