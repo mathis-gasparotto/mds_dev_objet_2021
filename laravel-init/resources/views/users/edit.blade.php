c452e30b73b637124f4d1172243c61e1b5442ec8
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2022-04-06 17:24:36 +0200

    correction of a typo

--- laravel-init/resources/views/users/edit.blade.php
+++ laravel-init/resources/views/users/edit.blade.php
@@ -14,14 +14,14 @@
                 @enderror
             </div>
             <div class="mb-3">
-                <label for="name" class="form-label form-required">Email:</label>
+                <label for="email" class="form-label form-required">Email:</label>
                 <input class="form-control @error('email') is-invalid @enderror" id="email" type="email" name="email" value="{{ $user->email }}" required>
                 @error('email')
                 <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
-                <label for="name" class="form-label form-required">Avatar URL:</label>
+                <label for="avatar_url" class="form-label form-required">Avatar URL:</label>
                 <input class="form-control @error('avatar_url') is-invalid @enderror" id="avatar_url" type="text" name="avatar_url" value="{{ $user->avatar_url }}" required>
                 @error('avatar_url')
                 <div class="alert alert-danger">{{ $message }}</div>
